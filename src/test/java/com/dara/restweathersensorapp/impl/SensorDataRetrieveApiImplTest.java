26f016ee23376103cb75194837291badbf41b0a8
DaraLeonard <darafleonard@gmail.com>
2021-12-21 15:17:54 +0000

    Adding tests to SensorDataRetrieveApiImplTest

--- src/test/java/com/dara/restweathersensorapp/impl/SensorDataRetrieveApiImplTest.java
+++ src/test/java/com/dara/restweathersensorapp/impl/SensorDataRetrieveApiImplTest.java
@@ -1,17 +1,47 @@
 package com.dara.restweathersensorapp.impl;
 
+import com.dara.restweathersensorapp.SensorRepository;
+import com.dara.restweathersensorapp.api.SensorDataRetrieveApi;
+import com.dara.restweathersensorapp.data.Sensor;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
 public class SensorDataRetrieveApiImplTest {
+
+    @Autowired
+    SensorRepository sensorRepository;
+
+    @BeforeEach
+    void setup() {
+        final Sensor.SensorBuilder sb = new Sensor.SensorBuilder();
+        final Sensor sensorA = sb.sensorId(1L).country("IE").city("Mayo").weatherMetricName("humidity").timeOfReading(LocalDateTime.MIN).valueOfReading(90D).build();
+        final Sensor sensorB = sb.sensorId(2L).country("IE").city("Galway").weatherMetricName("temperature").timeOfReading(LocalDateTime.MIN).valueOfReading(33.1).build();
+        final Sensor sensorC = sb.sensorId(3L).country("IE").city("Cork").weatherMetricName("humidity").timeOfReading(LocalDateTime.MIN).valueOfReading(60D).build();
+        final Sensor sensorD = sb.sensorId(4L).country("IE").city("Dublin").weatherMetricName("wind-speed").timeOfReading(LocalDateTime.MIN).valueOfReading(33.1).build();
+
+        sensorRepository.save(sensorA);
+        sensorRepository.save(sensorB);
+        sensorRepository.save(sensorC);
+        sensorRepository.save(sensorD);
+    }
 
     @Nested
     @DisplayName("getAllSensors")
     class GetAllSensorsTest {
         @Test
         public void GIVEN_callToGetAllSensors_WHEN_callExecuted_THEN_allRegisteredSensorsReturned() throws Exception {
+            SensorDataRetrieveApi sensorDataRetrieveApi = new SensorDataRetrieveApiImpl(sensorRepository);
 
+            assertEquals(4, sensorDataRetrieveApi.getAllSensors().getContent().size());
         }
     }
 
