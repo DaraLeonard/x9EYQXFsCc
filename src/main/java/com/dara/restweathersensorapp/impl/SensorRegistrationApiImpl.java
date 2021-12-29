84006c51f1248c6319f9a629a859eb088eec60d7
DaraLeonard <darafleonard@gmail.com>
2021-12-29 19:40:29 +0000

    SensorRegistrationApiImpl development started

--- src/main/java/com/dara/restweathersensorapp/impl/SensorRegistrationApiImpl.java
+++ src/main/java/com/dara/restweathersensorapp/impl/SensorRegistrationApiImpl.java
@@ -1,24 +1,43 @@
 package com.dara.restweathersensorapp.impl;
 
 
-import com.dara.restweathersensorapp.data.Sensor;
+import com.dara.restweathersensorapp.SensorRepository;
 import com.dara.restweathersensorapp.api.SensorRegistrationApi;
+import com.dara.restweathersensorapp.data.Sensor;
+import com.dara.restweathersensorapp.exception.DuplicateSensorException;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 public class SensorRegistrationApiImpl implements SensorRegistrationApi {
 
-    public SensorRegistrationApiImpl() {
+    private final SensorRepository sensorRepository;
 
+    public SensorRegistrationApiImpl(final SensorRepository sensorRepository) {
+        this.sensorRepository = sensorRepository;
     }
 
     @Override
     public ResponseEntity<?> addSensor(final Sensor sensor) {
-        return null;
+        if(sensorRepository.findById(sensor.getSensorId()).isPresent()) {
+            throw new DuplicateSensorException(sensor.getSensorId());
+        }
+
+        final EntityModel<Sensor> entityModel = EntityModel.of(sensorRepository.save(sensor),
+                linkTo(methodOn(SensorDataRetrieveApiImpl.class).getSensorById(sensor.getSensorId())).withSelfRel(),
+                linkTo(methodOn(SensorDataRetrieveApiImpl.class).getAllSensors()).withRel("sensors"));
+
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
     @Override
